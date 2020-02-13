cf14bf514262f572bb9e882a36e832a21422519a
YoomarXD <yooomarxd@gmail.com>
2020-02-12 20:18:32 -0600

    Create code_template.cpp
    Plantilla genérica que desde la que se programa

---
+++ code_template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> // Todas las librerías en una
+
+using namespace std; // Ahorra escribir std::cin, std::cout, etc
+
+// Mis #define, constantes o macros
+#define ENDL '\n'
+
+// Mis funciones
+void funcion() {
+}
+
+int main() {
+  // Vuelve más rápido cin y cout pero debo usar '\n' en lugar de endl
+  ios::sync_with_stdio(false);
+  cout.tie(nullptr);
+  
+  // Código fregón
+  cout << "A partir de aquí programas" << ENDL;
+  
+  return 0;
+}
